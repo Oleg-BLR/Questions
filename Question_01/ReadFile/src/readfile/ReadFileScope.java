b11526348f84f94984f4c05adca763261719558f
Oleg <Oleg-BLR@yandex.ru>
2022-11-30 13:47:57 +0300

    Вопрос в Question_01 в файле ReadFileScope.java

--- Question_01/ReadFile/src/readfile/ReadFileScope.java
+++ Question_01/ReadFile/src/readfile/ReadFileScope.java
@@ -6,14 +6,14 @@ import java.io.IOException;
 
 public class ReadFileScope {
     public static void main(String[] args) throws FileNotFoundException {
-        String path = "01/ReadFile/src/readfile/text.txt";
+        String path = "Question_01/ReadFile/src/readfile/text.txt";
         try {
             FileInputStream in = new FileInputStream(path);
             int biteCode = in.read();
             while (biteCode != -1) {
                 // здесь строки 16 и 17 вместе не работают
                 // только по отдельности. Почему?
-                System.out.print(biteCode + " ");
+//                System.out.print(biteCode + " ");
                 System.out.print((char) biteCode);
                 biteCode = in.read();
             }
